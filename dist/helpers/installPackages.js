295655e22e74a779ca5e26366bbb628ebb88876c
AquaGamingOfficial <67200746+AquaGamingOfficial@users.noreply.github.com>
2022-07-07 23:49:53 +0200

    Version change + TS compiled

--- dist/helpers/installPackages.js
+++ dist/helpers/installPackages.js
@@ -4,10 +4,9 @@ import { execa } from "../utils/execCmdAsync.js";
 import { logger } from "../utils/logger.js";
 export const installPackages = async ({ projectDir, packages, }) => {
     logger.info('Installing packages...');
-    console.log({ test: true, packages });
-    for (const [name] of packages) {
+    for (const name of packages) {
         const spinner = ora(`Installing ${name}...`).start();
-        await execa(`npm i ${name}`);
+        await execa(`npm i ${name}`, { cwd: projectDir });
         spinner.succeed(chalk.green(`Successfully installed ${chalk.green.bold(name)}`));
     }
     logger.info("");
